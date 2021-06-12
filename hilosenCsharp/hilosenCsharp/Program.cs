5d0abbe2d225d95366f8d0938da396c16d8c2fac
cesar978 <83042980+cesar978@users.noreply.github.com>
2021-06-12 10:05:11 -0500

    HILOS EN C#

---
+++ hilosenCsharp/hilosenCsharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hilosenCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumarThread sumarThread = new SumarThread();
+
+            Thread hilo1 = new Thread(new ParameterizedThreadStart(sumarThread.sumar));
+            Thread hilo2 = new Thread(new ParameterizedThreadStart(sumarThread.sumar));
+            Thread hilo3 = new Thread(new ParameterizedThreadStart(sumarThread.sumar));
+
+            hilo1.Name = "Hilo 1";
+            hilo2.Name = "Hilo 2";
+            hilo3.Name = "Hilo 3";
+
+            Int32[] parametros1 = { 2, 1 };
+            Int32[] parametros2 = { 2, 2 };
+            Int32[] parametros3 = { 2, 3 };
+
+           
+            Console.WriteLine("preparado los hilos");
+            hilo1.Start(parametros1);
+            hilo2.Start(parametros2);
+            hilo3.Start(parametros3);
+            Console.WriteLine("hilos procesando...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hilosenCsharp/hilosenCsharp/SumarThread.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace hilosenCsharp
+{
+    class SumarThread
+    {
+        public void sumar(object parametro)
+        {
+            Thread current = Thread.CurrentThread;
+            Int32[] numero = (Int32[])parametro;
+            Int32 resultado = numero[0] + numero[1];
+            Console.WriteLine("Durmiendo por" + resultado +
+                "segundo");
+            Thread.Sleep(resultado * 1000);
+            Console.WriteLine("El resultado de la suma es " +
+                numero[0] + " + " + numero[1] + " = " +
+                resultado);
+            Console.WriteLine("hilo " + current.Name + "finalizo su trabajo");
+        }
+    }
+}
